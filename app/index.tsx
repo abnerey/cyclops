a85788fc1def74f4473abcd06d20b5d10c5eaf02
ajuarez <ajuarez@bytesw.com>
2018-03-21 15:41:45 -0600

    develop: whatever

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from 'react-dom';
 import { ApplicationComponent } from "./components/ApplicationComponent";
 
 ReactDOM.render(
-    <ApplicationComponent title="Poke!" />,
+    <ApplicationComponent title="Pokemon!" />,
     document.getElementById("react-mount-point"),
 );
